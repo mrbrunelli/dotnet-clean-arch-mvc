32a02a74ee735f7895cc303b03ab9e19697b3605
mrbrunelli <matheus.brunelli@gmail.com>
2023-10-19 17:14:16 -0300

    add product unit tests

---
+++ CleanArchMvc.Domain.Tests/Mocks/ProductMockBuilder.cs
@@ -0,0 +1,78 @@
+﻿using Bogus;
+using Bogus.DataSets;
+using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Tests.Mocks
+{
+    public class ProductMockBuilder
+    {
+        private readonly Faker<ProductMockBuilder> _faker;
+        private int Id { get; set; }
+        private string Name { get; set; } = string.Empty;
+        private string Description { get; set; } = string.Empty;
+        private decimal Price { get; set; } = decimal.Zero;
+        private int Stock { get; set; }
+        private string ImageUrl { get; set; } = string.Empty;
+
+        public ProductMockBuilder()
+        {
+            _faker = new Faker<ProductMockBuilder>()
+                .RuleFor(p => p.Id, f => f.IndexFaker)
+                .RuleFor(p => p.Name, f => f.Commerce.ProductName())
+                .RuleFor(p => p.Description, f => f.Commerce.ProductDescription())
+                .RuleFor(p => p.Price, f => f.Random.Decimal(0.1m, 1000))
+                .RuleFor(p => p.Stock, f => f.Random.Int(1, 300))
+                .RuleFor(p => p.ImageUrl, f => f.Internet.Url());
+        }
+
+        public Product Build()
+        {
+            var mock = _faker.Generate();
+            return new Product(
+                mock.Id,
+                mock.Name,
+                mock.Description,
+                mock.Price,
+                mock.Stock,
+                mock.ImageUrl
+            );
+        }
+
+        public ProductMockBuilder WithName(string name)
+        {
+            _faker.RuleFor(p => p.Name, f => name);
+            return this;
+        }
+
+        public ProductMockBuilder WithDescription(string description)
+        {
+            _faker.RuleFor(p => p.Description, f => description);
+            return this;
+        }
+
+        public ProductMockBuilder WithPrice(decimal price)
+        {
+            _faker.RuleFor(p => p.Price, f => price);
+            return this;
+        }
+
+        public ProductMockBuilder WithStock(int stock)
+        {
+            _faker.RuleFor(p => p.Stock, f => stock);
+            return this;
+        }
+
+        public ProductMockBuilder WithImage(string imageUrl)
+        {
+            _faker.RuleFor(p => p.ImageUrl, f => imageUrl);
+            return this;
+        }
+    }
+}
---
+++ CleanArchMvc.Domain.Tests/ProductUnitTest.cs
@@ -0,0 +1,95 @@
+﻿using CleanArchMvc.Domain.Tests.Mocks;
+using CleanArchMvc.Domain.Validation;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Tests
+{
+    public class ProductUnitTest
+    {
+        private readonly ProductMockBuilder _productMock = new();
+
+        [Fact]
+        public void CreateProduct_ValidParameters_ResultValidObjectState()
+        {
+            Action action = () => _productMock.Build();
+            action.Should().NotThrow();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void CreateProduct_NullAndEmptyName_DomainExceptionInvalidName(string name)
+        {
+            Action action = () => _productMock.WithName(name).Build();
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid name. Name is required");
+        }
+
+        [Theory]
+        [InlineData("a")]
+        [InlineData("ab")]
+        [InlineData("12")]
+        public void CreateProduct_ShortName_DomainExceptionInvalidName(string name)
+        {
+            Action action = () => _productMock.WithName(name).Build();
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid name. Too short, minimum 3 characters");
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void CreateProduct_NullAndEmptyDescription_DomainExceptionInvalidDescription(string description)
+        {
+            Action action = () => _productMock.WithDescription(description).Build();
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid description. Description is required");
+        }
+
+        [Theory]
+        [InlineData("a")]
+        [InlineData("abc")]
+        [InlineData("1234")]
+        public void CreateProduct_ShortDescription_DomainExceptionInvalidDescription(string description)
+        {
+            Action action = () => _productMock.WithDescription(description).Build();
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid description. Too short, minimum 5 characters");
+        }
+
+        [Theory]
+        [InlineData(-0.5)]
+        [InlineData(-1)]
+        [InlineData(-10)]
+        public void CreateProduct_NegativePrice_DomainExceptionInvalidPrice(decimal price)
+        {
+            Action action = () => _productMock.WithPrice(price).Build();
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid price value");
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-10)]
+        public void CreateProduct_NegativeStock_DomainExceptionInvalidStock(int stock)
+        {
+            Action action = () => _productMock.WithStock(stock).Build();
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid stock value");
+        }
+
+        [Theory]
+        [InlineData(@"
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        )]
+        public void CreateProduct_LongImageLocation_DomainExceptionInvalidImageLocation(string image)
+        {
+            Action action = () => _productMock.WithImage(image).Build();
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid image location. Too long, maximum 250 characters");
+        }
+    }
+}
