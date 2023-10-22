08650a6a0c22f4336cd3d8fb6beae3687ef3e62e
Matheus Ricardo Brunelli <matheus.brunelli@gmail.com>
2023-10-22 09:40:12 -0300

    add product service

---
+++ CleanArchMvc.Application/Interfaces/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchMvc.Application.DTOs;
+
+namespace CleanArchMvc.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDTO>> GetAll();
+        Task<ProductDTO> GetById(int? id);
+        Task<ProductDTO> GetByIdIncludeCategory(int? id);\
+        Task Add(ProductDTO productDTO);
+        Task Update(ProductDTO productDTO);
+        Task Remove(int? id);
+    }
+}
---
+++ CleanArchMvc.Application/Services/ProductService.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+
+namespace CleanArchMvc.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _repository;
+        private readonly IMapper _mapper;
+
+        public ProductService(IProductRepository repository, IMapper mapper)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _mapper = mapper;
+        }
+
+        public async Task Add(ProductDTO productDTO)
+        {
+            var product = _mapper.Map<Product>(productDTO);
+            await _repository.CreateAsync(product);
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetAll()
+        {
+            var products = await _repository.GetAllAsync();
+            return _mapper.Map<IEnumerable<ProductDTO>>(products);
+        }
+
+        public async Task<ProductDTO> GetById(int? id)
+        {
+            var product = await _repository.GetByIdAsync(id);
+            return _mapper.Map<ProductDTO>(product);
+        }
+
+        public async Task<ProductDTO> GetByIdIncludeCategory(int? id)
+        {
+            var product = await _repository.GetByIdIncludeCategoryAsync(id);
+            return _mapper.Map<ProductDTO>(product);
+        }
+
+        public async Task Remove(int? id)
+        {
+            var product = await _repository.GetByIdAsync(id);
+            await _repository.RemoveAsync(product);
+        }
+
+        public async Task Update(ProductDTO productDTO)
+        {
+            var product = _mapper.Map<Product>(productDTO);
+            await _repository.UpdateAsync(product);
+        }
+    }
+}
