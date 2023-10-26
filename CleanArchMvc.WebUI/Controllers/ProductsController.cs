a4cecf3fa0c9abd18462189a62a89c1e4acb01f1
mrbrunelli <matheus.brunelli@gmail.com>
2023-10-26 11:11:45 -0300

    add products controller

---
+++ CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _service;
+
+        public ProductsController(IProductService service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _service.GetAll();
+            return View(products);
+        }
+    }
+}
